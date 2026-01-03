7c8102d4ccd158b11bc5467188616f8bd01cccbe
Abdelrahman Mohamed <obada.semary@gmail.com>
2026-01-04 00:18:43 +0300

    Add CarouselView component for character browsing

---
+++ Momentum/Components/CarouselView/CarouselView.swift
@@ -0,0 +1,68 @@
+//
+//  CarouselView.swift
+//  Momentum
+//
+//  Created by Abdelrahman Mohamed on 03.01.2026.
+//
+
+import SwiftUI
+
+struct CarouselView: View {
+    
+    let characters: [CharactersResponse]
+    
+    @State private var currentIndex: Int = 0
+    
+    var body: some View {
+        Group {
+            if characters.isEmpty {
+                EmptyView()
+            } else {
+                TabView(selection: $currentIndex) {
+                    ForEach(characters, id: \.id) { character in
+                        CarouselCard(character: character)
+                            .buttonStyle(.plain)
+                            .padding(.horizontal)
+                    }
+                }
+                .tabViewStyle(.page(indexDisplayMode: .automatic))
+                .frame(height: 220)
+                .clipShape(
+                    RoundedRectangle(cornerRadius: 15, style: .continuous)
+                )
+                .padding(.top)
+            }
+        }
+    }
+}
+
+#Preview {
+    CarouselView(
+        characters: [
+            .init(
+                id: 1,
+                name: "Obada",
+                species: "Human",
+                image: URL(string: "https://rickandmortyapi.com/api/character/avatar/1.jpeg")
+            ),
+            .init(
+                id: 2,
+                name: "Sara",
+                species: "Human",
+                image: URL(string: "https://rickandmortyapi.com/api/character/avatar/2.jpeg")
+            ),
+            .init(
+                id: 3,
+                name: "Nazli",
+                species: "Human",
+                image: URL(string: "https://rickandmortyapi.com/api/character/avatar/3.jpeg")
+            ),
+            .init(
+                id: 4,
+                name: "Omar",
+                species: "Human",
+                image: URL(string: "https://rickandmortyapi.com/api/character/avatar/4.jpeg")
+            )
+        ]
+    )
+}
