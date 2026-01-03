be6545400df3e9d0e145f517d3f647c56c163c74
Abdelrahman Mohamed <obada.semary@gmail.com>
2026-01-04 01:06:34 +0300

    Add CharacterView component and update FeedView with character carousel
    - Create reusable CharacterView component for displaying character info
    - Update FeedEntity with character property and default initializer
    - Integrate CharacterView into FeedView carousel
    - Add test coverage for FeedEntity defaults

---
+++ Momentum/Components/CharacterView.swift
@@ -0,0 +1,18 @@
+//
+//  CharacterView.swift
+//  Momentum
+//
+//  Created by Abdelrahman Mohamed on 04.01.2026.
+//
+
+import SwiftUI
+
+struct CharacterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CharacterView()
+}
