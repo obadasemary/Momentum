8e867e90668aa5f42ed2e9d8d7e89045100edfab
Abdelrahman Mohamed <obada.semary@gmail.com>
2026-01-03 21:38:40 +0300

    Add data loading and error handling to FeedViewModel

--- Momentum/FeedView/FeedViewModel.swift
+++ Momentum/FeedView/FeedViewModel.swift
@@ -13,8 +13,36 @@ final class FeedViewModel {
     private let feedUseCase: FeedUseCaseProtocol
     
     private(set) var characters: [CharactersResponse] = []
+    private(set) var isLoading: Bool = false
+    private(set) var errorMessage: String?
     
     init(feedUseCase: FeedUseCaseProtocol) {
         self.feedUseCase = feedUseCase
+    }
+    
+    func loadData() async {
+        
+        isLoading = true
+        
+        do {
+            try await fetchFeed()
+        } catch {
+            errorMessage  = error.localizedDescription
+        }
+        
+        isLoading = false
+    }
+}
+
+extension FeedViewModel {
+    
+    func fetchFeed() async throws {
+        guard let url = Constants.url else {
+            isLoading = false
+            return
+        }
+        
+        let response = try await feedUseCase.fetchFeed(url: url)
+        characters = response.results
     }
 }
