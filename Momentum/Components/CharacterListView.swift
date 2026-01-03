07cf057ff584190314358ac71ea883c2ed1ef401
Abdelrahman Mohamed <obada.semary@gmail.com>
2026-01-04 02:11:41 +0300

    Implement character list with ForEach iteration

--- Momentum/Components/CharacterListView.swift
+++ Momentum/Components/CharacterListView.swift
@@ -8,11 +8,43 @@
 import SwiftUI
 
 struct CharacterListView: View {
+    
+    let characters: [CharactersResponse]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ForEach(characters, id: \.id) { character in
+            CharacterView(character: character)
+        }
     }
 }
 
 #Preview {
-    CharacterListView()
+    CharacterListView(
+        characters: [
+            .init(
+                id: 1,
+                name: "Obada",
+                species: "Human",
+                image: URL(string: "https://rickandmortyapi.com/api/character/avatar/1.jpeg")
+            ),
+            .init(
+                id: 2,
+                name: "Sara",
+                species: "Human",
+                image: URL(string: "https://rickandmortyapi.com/api/character/avatar/2.jpeg")
+            ),
+            .init(
+                id: 3,
+                name: "Nazli",
+                species: "Human",
+                image: URL(string: "https://rickandmortyapi.com/api/character/avatar/3.jpeg")
+            ),
+            .init(
+                id: 4,
+                name: "Omar",
+                species: "Human",
+                image: URL(string: "https://rickandmortyapi.com/api/character/avatar/4.jpeg")
+            )
+        ]
+    )
 }
