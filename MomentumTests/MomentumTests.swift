bdd51130de0e4e90aa7662994ffb50a0bb657800
Abdelrahman Mohamed <obada.semary@gmail.com>
2026-01-04 01:07:38 +0300

    Add FeedViewModel unit tests for success and failure cases

--- MomentumTests/MomentumTests.swift
+++ MomentumTests/MomentumTests.swift
@@ -6,11 +6,70 @@
 //
 
 import Testing
+@testable import Momentum
 
 struct MomentumTests {
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    @MainActor
+    @Test("Test Fetch Feed With Success")
+    func fetchFeedWithSuccess() async throws {
+        
+        let response = [
+            CharactersResponse(
+                id: 1,
+                name: "Obada",
+                species: "Human",
+                image: nil
+            ),
+            CharactersResponse(
+                id: 1,
+                name: "Obada",
+                species: "Human",
+                image: nil
+            )
+        ]
+        
+        let makeSut = {
+            MockFeedUseCase(
+                result:
+                        .success(
+                            FeedEntity(
+                                info: .init(count: 1, pages: 1),
+                                results: response
+                            )
+                        )
+            )
+        }
+        
+        let viewModel = FeedViewModel(feedUseCase: makeSut())
+        
+        await viewModel.loadData()
+        
+        #expect(viewModel.characters == response)
     }
+    
+    @MainActor
+    @Test("Test Fetch Feed With Failure")
+    func fetchFeedWithFailure() async throws {
+        
+        let makeSut = {
+            MockFeedUseCase(
+                result:
+                        .failure(MockError.stub)
+            )
+        }
+        
+        let viewModel = FeedViewModel(feedUseCase: makeSut())
+        
+        await viewModel.loadData()
+        
+        #expect(viewModel.characters.isEmpty)
+    }
+}
 
+private extension MomentumTests {
+    
+    enum MockError: Error {
+        case stub
+    }
 }
