c83eeff6a5747811f52b0471a7be43be554ed747
Abdelrahman Mohamed <obada.semary@gmail.com>
2026-01-03 23:24:15 +0300

    Add CarouselCard view component

---
+++ Momentum/Components/CarouselView/CarouselCard.swift
@@ -0,0 +1,18 @@
+//
+//  CarouselCard.swift
+//  Momentum
+//
+//  Created by Abdelrahman Mohamed on 03.01.2026.
+//
+
+import SwiftUI
+
+struct CarouselCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CarouselCard()
+}
